a407b226dc4784e4b53731c83fd0c727a60c25c4
Arnab-108 <arnabadhikary007@gmail.com>
2024-02-06 16:18:02 +0530

    3rd

--- Frontend/noteapp/src/Component/Body.jsx
+++ Frontend/noteapp/src/Component/Body.jsx
@@ -1,9 +1,16 @@
+import { Box, Image } from '@chakra-ui/react'
 import React from 'react'
 
 export const Body = () => {
   return (
     <>
-    
+        <Box height={"80vh"} width={"100%"} >
+            <Image 
+                boxSize={"100%"}
+                objectFit={"cover"}
+                src='https://simplenoteblog.files.wordpress.com/2020/07/img_simplenote_hero.png'
+            />
+        </Box>
     </>
   )
 }
--- Frontend/noteapp/src/Pages/Homepage.jsx
+++ Frontend/noteapp/src/Pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from '../Component/Navbar'
 import { Header} from '../Component/Heading'
 import { Box } from '@chakra-ui/react'
+import { Body } from '../Component/Body'
 
 export const Homepage = () => {
   return (
@@ -11,6 +12,9 @@ export const Homepage = () => {
         </Box>
         <Box pos={"relative"} top={"5vh"}>
             <Header />
+        </Box>
+        <Box pos={"relative"} top={"9vh"}>
+            <Body />
         </Box>
     </>
   )
