2cbe9a056dd138e37a3ad929de35b9b1a767fa4f
Arnab-108 <arnabadhikary007@gmail.com>
2024-02-11 15:35:38 +0530

    7th-1

--- Backend/Controller/user.controller.js
+++ Backend/Controller/user.controller.js
@@ -7,14 +7,14 @@ const Signup = async (req, res) => {
 
     const user = await userModel.findOne({ email: email })
     if (user) {
-        res.status(200).send({ mag: "User already exists. Please login!" })
+        res.status(200).send({ msg: "User already exists. Please login!" })
     }
     else {
         try {
             bcrypt.hash(password, 5, async (err, hash) => {
                 const data = userModel({ name, email, password: hash, age, avatar: "https://i.pinimg.com/736x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg" })
                 await data.save()
-                res.status(200).send({ mag: "User registered successfully", user: data })
+                res.status(200).send({ msg: "User registered successfully", user: data })
             })
         } catch (error) {
             res.status(404).send(error)
@@ -50,4 +50,15 @@ const Login = async (req, res) => {
 
 }
 
-module.exports = { Signup, Login }
+const SingleUser= async(req,res)=>{
+    const {id} = req.params
+    try {
+         const data = await userModel.find({_id:id})
+         res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send({err:error})
+    }
+}
+
+
+module.exports = { Signup, Login , SingleUser }
--- Backend/Route/user.route.js
+++ Backend/Route/user.route.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const { Signup, Login } = require("../Controller/user.controller")
+const { Signup, Login, SingleUser } = require("../Controller/user.controller")
 const authRoute = express.Router()
 
 authRoute.post("/signup", Signup)
 authRoute.post("/login" , Login)
-
+authRoute.get("/:id", SingleUser)
 module.exports={authRoute}
