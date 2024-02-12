3bc2c3503374d83f1dcf0b11c3b027229b9128e0
Arnab-108 <arnabadhikary007@gmail.com>
2024-02-12 15:01:29 +0530

    8th-2 middleware added in routes

--- Backend/Route/user.route.js
+++ Backend/Route/user.route.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const { Signup, Login, SingleUser, UpdateUser } = require("../Controller/user.controller")
 const { uploads } = require("../Middleware/multer.middleware")
+const { auth } = require("../Middleware/auth.middleware")
 const authRoute = express.Router()
 
 authRoute.post("/signup", Signup)
 authRoute.post("/login" , Login)
+authRoute.use(auth)
 authRoute.get("/:id", SingleUser)
 authRoute.patch("/:id" , uploads.single('avatar') , UpdateUser)
 module.exports={authRoute}
