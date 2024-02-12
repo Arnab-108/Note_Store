8485a8506fa39b6717c3315692455858fe31e854
Arnab-108 <arnabadhikary007@gmail.com>
2024-02-12 11:18:59 +0530

    8th

---
+++ Backend/Middleware/multer.middleware.js
@@ -0,0 +1,47 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './files')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now()
+      cb(null, uniqueSuffix+file.originalname)
+    }
+  })
+
+
+const uploads = multer({
+    storage:storage,
+    fileFilter: function(req,file,cb){
+        if(file.mimetype==='image/png'){
+            cb(null,true)
+        }
+        else if(file.mimetype==="image/jpeg"){
+            cb(null,true)
+        }
+        else if(file.mimetype==='application/pdf'){
+            cb(null,true)
+        }
+        else if(file.mimetype === 'application/msword'){
+            cb(null,true)
+        }
+        else if(file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'){
+            cb(null,true)
+        }
+        else if(file.mimetype === 'application/vnd.ms-excel' || file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){
+            cb(null,true)
+        }
+        else{
+            cd(new Error('Files not supported!'))
+            cb(null,false)
+        }
+    },
+    limits:{
+        fileSize: 1024*1024*2
+    }
+
+})
+
+
+module.exports={uploads}
