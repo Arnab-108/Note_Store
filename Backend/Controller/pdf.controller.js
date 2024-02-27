8100d04001c9823a9825deaa13d87c383c31fe9e
Arnab-108 <arnabadhikary007@gmail.com>
2024-02-27 14:56:54 +0530

    18th

--- Backend/Controller/pdf.controller.js
+++ Backend/Controller/pdf.controller.js
@@ -1,6 +1,6 @@
-const {pdfModel} = require("../Model/pdf.model")
+const { pdfModel } = require("../Model/pdf.model")
 
-const Upload = async(req,res)=>{
+const Upload = async (req, res) => {
     try {
         const note = new pdfModel({
             user_Id: req.user_Id, // Make sure these values are available in req.body
@@ -10,11 +10,44 @@ const Upload = async(req,res)=>{
             file: req.file.filename
         })
         await note.save()
-        res.status(200).send({msg:"Successfully Uploaded!!" , files:note})
+        res.status(200).send({ msg: "Successfully Uploaded!!", files: note })
+    } catch (error) {
+        res.status(404).send({ err: error.message })
+    }
+}
+
+const GET = async (req, res) => {
+    try {
+        const data = await pdfModel.find({ user_Id: req.user_Id })
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send({err:error.message})
+    }
+
+}
+
+const UPDATE = async(req,res)=>{
+    const {id} = req.params
+    try {
+        let file = ""
+        if(req.file){
+            file=`files/${req.file.filename}`
+        }
+        req.body.file = file
+
+        const userinfo = pdfModel.find({_id:id})
+        console.log(userinfo, "user")
+        const persentImg = userinfo.file
+        console.log(persentImg, "img")
+        if (persentImg) {
+            fs.unlinkSync(DIR + persentImg)
+        }
+
+        const updateItem = await pdfModel.findByIdAndUpdate({ _id: id }, req.body)
+        res.status(200).send({ msg: `Successfully updated the ${id}!`, data: updateItem })
     } catch (error) {
         res.status(404).send({err:error.message})
     }
 }
 
-
-module.exports={Upload}
+module.exports = { Upload,GET,UPDATE }
--- Backend/Route/pdf.router.js
+++ Backend/Route/pdf.router.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const { uploads } = require("../Middleware/multer.middleware")
-const { Upload } = require("../Controller/pdf.controller")
+const { Upload, GET, UPDATE } = require("../Controller/pdf.controller")
 const { auth } = require("../Middleware/auth.middleware")
 
 const noteRouter = express.Router()
 noteRouter.use(auth)
 noteRouter.post("/upload" ,uploads.single('file'), Upload)
-
-
+noteRouter.get("/", GET)
+noteRouter.patch("/:id" , uploads.single('file') , UPDATE )
 module.exports={noteRouter}
